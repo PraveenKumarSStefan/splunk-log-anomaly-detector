159e153b8cff6029e82b3acf6c08916e8869993f
Praveenkumar <63362947+PraveenKumarSStefan@users.noreply.github.com>
2026-03-28 14:39:08 +0530

    Add src/anomaly_scorer.py

--- src/anomaly_scorer.py
+++ src/anomaly_scorer.py
@@ -1,12 +1,6 @@
 class AnomalyScorer:
     def score_new_error(self, r):
-        s = 50
         c = int(r.get("count",0))
-        if c > 100: s += 30
-        elif c > 50: s += 20
-        elif c > 20: s += 10
-        return min(s,100)
-    def score_spike(self, factor):
-        if factor > 10: return 90
-        if factor > 5: return 70
+        if c > 100: return 80
+        if c > 50: return 70
         return 50
