ad8a74fd23f4ca4e7e8fc33d9fc1f41c91054671
Praveenkumar <63362947+PraveenKumarSStefan@users.noreply.github.com>
2026-03-28 14:39:10 +0530

    Add src/dedup_cache.py

--- src/dedup_cache.py
+++ src/dedup_cache.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 class DedupCache:
-    def __init__(self, ttl_minutes=30):
-        self._cache = {}
-        self._ttl = timedelta(minutes=ttl_minutes)
-    def is_duplicate(self, key):
-        if key in self._cache and datetime.now() - self._cache[key] < self._ttl: return True
-        if key in self._cache: del self._cache[key]
+    def __init__(self, ttl=30):
+        self._cache={}; self._ttl=timedelta(minutes=ttl)
+    def is_duplicate(self, k):
+        if k in self._cache and datetime.now()-self._cache[k]<self._ttl: return True
+        if k in self._cache: del self._cache[k]
         return False
-    def mark(self, key): self._cache[key] = datetime.now()
+    def mark(self, k): self._cache[k]=datetime.now()
